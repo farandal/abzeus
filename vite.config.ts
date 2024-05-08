479f21e90a9f2c8ed4e4379871f15f4da0060153
Francisco Aranda <farandal@gmail.com>
2024-05-07 21:28:17 -0400

    fix: build issue with abzeus lib

--- vite.config.ts
+++ vite.config.ts
@@ -64,7 +64,12 @@ export default defineConfig(({ command }) => {
               minify: isBuild,
               outDir: 'dist-electron/main',
               rollupOptions: {
-                external: [...Object.keys('dependencies' in pkg ? pkg.dependencies : {}),"abzeus"],
+                external: [...Object.keys('dependencies' in pkg ? pkg.dependencies : {}),"abzeus/src"],
+                output: {
+                    globals: {
+                      'abzeus/src': 'abzeus'
+                    }
+                  }
               },
             },
           },
